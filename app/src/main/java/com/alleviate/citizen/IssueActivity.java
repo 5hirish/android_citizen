44c6ed931f96c24c21900866a31f9d21efe5a493
Shirish Kadam <shirishkadam35@yahoo.com>
2017-03-18 21:04:40 +0530

    Sanitized code

--- app/src/main/java/com/alleviate/citizen/IssueActivity.java
+++ app/src/main/java/com/alleviate/citizen/IssueActivity.java
@@ -40,81 +40,7 @@ public class IssueActivity extends AppCompatActivity {
     String option[] = new String[3];
     int id,imageid,score,level,basescore;
 
-    int images[] = {R.mipmap.ic_0,
-                    R.mipmap.ic_1,
-                    R.mipmap.ic_2,
-                    R.mipmap.ic_3,
-                    R.mipmap.ic_4,
-                    R.mipmap.ic_5,
-                    R.mipmap.ic_6,
-                    R.mipmap.ic_7,
-                    R.mipmap.ic_8,
-                    R.mipmap.ic_9,
-                    R.mipmap.ic_10,
-                    R.mipmap.ic_11,
-                    R.mipmap.ic_12,
-                    R.mipmap.ic_13,
-                    R.mipmap.ic_14,
-                    R.mipmap.ic_15,
-                    R.mipmap.ic_16,
-                    R.mipmap.ic_17,
-                    R.mipmap.ic_18,
-                    R.mipmap.ic_19,
-                    R.mipmap.ic_20,
-                    R.mipmap.ic_21,
-                    R.mipmap.ic_22,
-                    R.mipmap.ic_23,
-                    R.mipmap.ic_24,
-                    R.mipmap.ic_25,
-                    R.mipmap.ic_26,
-                    R.mipmap.ic_27,
-                    R.mipmap.ic_28,
-            R.mipmap.ic_29,
-            R.mipmap.ic_30,
-            R.mipmap.ic_31,
-            R.mipmap.ic_32,
-            R.mipmap.ic_33,
-            R.mipmap.ic_34,
-            R.mipmap.ic_35,
-            R.mipmap.ic_36,
-            R.mipmap.ic_37,
-            R.mipmap.ic_38,
-            R.mipmap.ic_39,
-            R.mipmap.ic_40,
-            R.mipmap.ic_41,
-            R.mipmap.ic_42,
-            R.mipmap.ic_43,
-            R.mipmap.ic_44,
-            R.mipmap.ic_45,
-            R.mipmap.ic_46,
-            R.mipmap.ic_47,
-            R.mipmap.ic_48,
-            R.mipmap.ic_49,
-            R.mipmap.ic_50,
-            R.mipmap.ic_51,
-            R.mipmap.ic_52,
-            R.mipmap.ic_53,
-            R.mipmap.ic_54,
-            R.mipmap.ic_55,
-            R.mipmap.ic_56,
-            R.mipmap.ic_57,
-            R.mipmap.ic_58,
-            R.mipmap.ic_59,
-            R.mipmap.ic_60,
-            R.mipmap.ic_61,
-            R.mipmap.ic_62,
-            R.mipmap.ic_63,
-            R.mipmap.ic_64,
-            R.mipmap.ic_65,
-            R.mipmap.ic_66,
-            R.mipmap.ic_67,
-            R.mipmap.ic_68,
-            R.mipmap.ic_69,
-            R.mipmap.ic_70,
-            R.mipmap.ic_71,
-            R.mipmap.ic_72,
-            R.mipmap.ic_73
-    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -130,21 +56,21 @@ public class IssueActivity extends AppCompatActivity {
         SQLiteHelper db = new SQLiteHelper(getApplicationContext());
         SQLiteDatabase dbr = db.getReadableDatabase();
 
-        String[] col = {db.dbCZ_Id,db.dbCZ_Issue,db.dbCZ_OptA,db.dbCZ_OptB,db.dbCZ_OptC,db.dbCZ_Answer,db.dbCZ_Status,db.dbCZ_Explanation,db.dbCZ_ImageId};
-        String where = db.dbCZ_Status+" = 'N'";
+        //String[] col = {db.dbCZ_Id,db.dbCZ_Issue,db.dbCZ_OptA,db.dbCZ_OptB,db.dbCZ_OptC,db.dbCZ_Answer,db.dbCZ_Status,db.dbCZ_Explanation,db.dbCZ_ImageId};
+        String where = SQLiteHelper.dbCZ_Status +" = 'N'";
 
-        Cursor cur =dbr.query(db.dbCZ_table_Issue,col,where,null,null,null,null,"1");
+        Cursor cur =dbr.query(SQLiteHelper.dbCZ_table_Issue, null, where, null, null, null, null, "1");
 
         if(cur!=null){
             while (cur.moveToNext()){
-                id =cur.getInt(cur.getColumnIndex(db.dbCZ_Id));
-                issue =cur.getString(cur.getColumnIndex(db.dbCZ_Issue));
-                option[0] =cur.getString(cur.getColumnIndex(db.dbCZ_OptA));
-                option[1] =cur.getString(cur.getColumnIndex(db.dbCZ_OptB));
-                option[2] =cur.getString(cur.getColumnIndex(db.dbCZ_OptC));
-                answer =cur.getString(cur.getColumnIndex(db.dbCZ_Answer));
-                explanation =cur.getString(cur.getColumnIndex(db.dbCZ_Explanation));
-                imageid =cur.getInt(cur.getColumnIndex(db.dbCZ_ImageId));
+                id =cur.getInt(cur.getColumnIndex(SQLiteHelper.dbCZ_Id));
+                issue =cur.getString(cur.getColumnIndex(SQLiteHelper.dbCZ_Issue));
+                option[0] =cur.getString(cur.getColumnIndex(SQLiteHelper.dbCZ_OptA));
+                option[1] =cur.getString(cur.getColumnIndex(SQLiteHelper.dbCZ_OptB));
+                option[2] =cur.getString(cur.getColumnIndex(SQLiteHelper.dbCZ_OptC));
+                answer =cur.getString(cur.getColumnIndex(SQLiteHelper.dbCZ_Answer));
+                explanation =cur.getString(cur.getColumnIndex(SQLiteHelper.dbCZ_Explanation));
+                imageid =cur.getInt(cur.getColumnIndex(SQLiteHelper.dbCZ_ImageId));
 
             }cur.close();
         }
@@ -160,7 +86,7 @@ public class IssueActivity extends AppCompatActivity {
         final RadioGroup radioGroup = new RadioGroup(this);
         radioGroup.setOrientation(RadioGroup.VERTICAL);//or RadioGroup.VERTICAL
 
-        for(int i=0; i<N_options; i++){
+        for(int i = 0; i < N_options; i++){
             radioButtons[i]  = new RadioButton(this);
             radioGroup.addView(radioButtons[i]);
             radioButtons[i].setTextSize(18);
@@ -173,7 +99,7 @@ public class IssueActivity extends AppCompatActivity {
 
         View cardView = (View)findViewById(R.id.card_view);
         ImageView imageView = (ImageView)findViewById(R.id.card_thumbnail_image);
-        imageView.setImageResource(images[imageid]);
+        imageView.setImageResource(IconRes.images[imageid]);
 
         TextView tvissue = (TextView)cardView.findViewById(R.id.issue);
         tvissue.setText(issue);
@@ -267,7 +193,7 @@ public class IssueActivity extends AppCompatActivity {
     }
 
     private void next_issue() {
-        final SharedPreferences citizen = getSharedPreferences("CitiZen",MODE_PRIVATE);
+        final SharedPreferences citizen = getSharedPreferences("CitiZen", MODE_PRIVATE);
         final SharedPreferences.Editor citizen_editor = citizen.edit();
         score = citizen.getInt("Score", 0);
         level = citizen.getInt("Level", 1);
@@ -276,21 +202,21 @@ public class IssueActivity extends AppCompatActivity {
         SQLiteHelper db = new SQLiteHelper(getApplicationContext());
         SQLiteDatabase dbr = db.getReadableDatabase();
 
-        String[] col = {db.dbCZ_Id,db.dbCZ_Issue,db.dbCZ_OptA,db.dbCZ_OptB,db.dbCZ_OptC,db.dbCZ_Answer,db.dbCZ_Status,db.dbCZ_Explanation,db.dbCZ_ImageId};
-        String where = db.dbCZ_Status+" = 'N'";
+        //String[] col = new String[]{SQLiteHelper.dbCZ_Id, SQLiteHelper.dbCZ_Issue, SQLiteHelper.dbCZ_OptA, db.dbCZ_OptB, db.dbCZ_OptC, db.dbCZ_Answer, db.dbCZ_Status, db.dbCZ_Explanation, db.dbCZ_ImageId};
+        String where = SQLiteHelper.dbCZ_Status +" = 'N'";
 
-        Cursor cur =dbr.query(db.dbCZ_table_Issue,col,where,null,null,null,null,"1");
+        Cursor cur =dbr.query(SQLiteHelper.dbCZ_table_Issue, null, where, null, null, null, null, "1");
 
         if(cur!=null){
             while (cur.moveToNext()){
-                id =cur.getInt(cur.getColumnIndex(db.dbCZ_Id));
-                issue =cur.getString(cur.getColumnIndex(db.dbCZ_Issue));
-                option[0] =cur.getString(cur.getColumnIndex(db.dbCZ_OptA));
-                option[1] =cur.getString(cur.getColumnIndex(db.dbCZ_OptB));
-                option[2] =cur.getString(cur.getColumnIndex(db.dbCZ_OptC));
-                answer =cur.getString(cur.getColumnIndex(db.dbCZ_Answer));
-                explanation =cur.getString(cur.getColumnIndex(db.dbCZ_Explanation));
-                imageid =cur.getInt(cur.getColumnIndex(db.dbCZ_ImageId));
+                id =cur.getInt(cur.getColumnIndex(SQLiteHelper.dbCZ_Id));
+                issue =cur.getString(cur.getColumnIndex(SQLiteHelper.dbCZ_Issue));
+                option[0] =cur.getString(cur.getColumnIndex(SQLiteHelper.dbCZ_OptA));
+                option[1] =cur.getString(cur.getColumnIndex(SQLiteHelper.dbCZ_OptB));
+                option[2] =cur.getString(cur.getColumnIndex(SQLiteHelper.dbCZ_OptC));
+                answer =cur.getString(cur.getColumnIndex(SQLiteHelper.dbCZ_Answer));
+                explanation =cur.getString(cur.getColumnIndex(SQLiteHelper.dbCZ_Explanation));
+                imageid =cur.getInt(cur.getColumnIndex(SQLiteHelper.dbCZ_ImageId));
 
             }cur.close();
         }
@@ -319,7 +245,7 @@ public class IssueActivity extends AppCompatActivity {
 
         View cardView = (View)findViewById(R.id.card_view);
         ImageView imageView = (ImageView)findViewById(R.id.card_thumbnail_image);
-        imageView.setImageResource(images[imageid]);
+        imageView.setImageResource(IconRes.images[imageid]);
 
         TextView tvissue = (TextView)cardView.findViewById(R.id.issue);
         tvissue.setText(issue);
@@ -373,11 +299,11 @@ public class IssueActivity extends AppCompatActivity {
                         SQLiteDatabase dbw = db.getWritableDatabase();
 
                         ContentValues update_values = new ContentValues();
-                        update_values.put(db.dbCZ_Status,"Y");
+                        update_values.put(SQLiteHelper.dbCZ_Status,"Y");
 
-                        String where = db.dbCZ_Id+" = "+id;
+                        String where = SQLiteHelper.dbCZ_Id +" = "+id;
 
-                        dbw.update(db.dbCZ_table_Issue, update_values, where, null);
+                        dbw.update(SQLiteHelper.dbCZ_table_Issue, update_values, where, null);
 
                         next_issue();
                     }
